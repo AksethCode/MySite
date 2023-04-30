1bc6fddb433e66f46513a8c0b887ec227a81d07f
Akshay Seth <akshayseth@Akshays-iMac.local>
2023-04-30 19:54:49 +0530

    Third Commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+app.use('/images', express.static('images'))
+app.use('/CSS', express.static('css'))
 app.get ('/', (req,res) => {
     res.sendFile(__dirname+"/index.html")
 })
